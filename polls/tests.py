53c6c8c391108d1a15c15a31fda1bf57f454c9ae
Ph. Nam Anh <na.ph0304@gmail.com>
2023-08-09 02:15:06 +0700

    FE Base first commit

---
+++ polls/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase, SimpleTestCase
+
+# Create your tests here.
+class SimpleTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
